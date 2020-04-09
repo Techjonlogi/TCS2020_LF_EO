02ef032cbb3bbf19446bb96290cccb85fdf094fa
Alejandro <alex-blanco-00@hotmail.com>
2020-04-09 14:44:41 -0500

    Menú Delegaciones Municipales
    Avance en el menú de delegaciones municipales

--- Sistema_DelegacionMunicipal/Sistema_DelegacionMunicipal/MainWindow.xaml.cs
+++ Sistema_DelegacionMunicipal/Sistema_DelegacionMunicipal/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Sistema_DelegacionMunicipal.ViewController;
 
 namespace Sistema_DelegacionMunicipal
 {
@@ -20,9 +21,99 @@ namespace Sistema_DelegacionMunicipal
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int botonSeleccionado = 0;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+
+        private void BtnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void BtnConductores_Click(object sender, RoutedEventArgs e)
+        {
+            if (botonSeleccionado != 1)
+            {
+                cambiarBoton(1);
+                GridPrincipal.Children.Clear();
+                GridPrincipal.Children.Add(new Conductor());
+            }
+        }
+
+        private void BtnLevantarReporte_Click(object sender, RoutedEventArgs e)
+        {
+            if (botonSeleccionado != 2)
+            {
+                cambiarBoton(2);
+                GridPrincipal.Children.Clear();
+                GridPrincipal.Children.Add(new LevantarReporte());
+            }
+        }
+
+        private void BtnHistorialReportes_Click(object sender, RoutedEventArgs e)
+        {
+            if (botonSeleccionado != 3)
+            {
+                cambiarBoton(3);
+                GridPrincipal.Children.Clear();
+                GridPrincipal.Children.Add(new HistorialReportes());
+            }
+        }
+
+        private void btnChat_Click(object sender, RoutedEventArgs e)
+        {
+            if (botonSeleccionado != 4)
+            {
+                cambiarBoton(4);
+                GridPrincipal.Children.Clear();
+                GridPrincipal.Children.Add(new Chat());
+            }
+        }
+
+
+        private void cambiarBoton(int seleccionado)
+        {
+            btnConductores.Background = Brushes.Transparent;
+            btnLevantarReporte.Background = Brushes.Transparent;
+            btnHistorialReportes.Background = Brushes.Transparent;
+            btnChat.Background = Brushes.Transparent;
+
+
+            switch (seleccionado)
+            {
+                case (0):
+
+                    break;
+
+                case (1):
+                    botonSeleccionado = 1;
+                    btnConductores.Background = Brushes.White;
+                    break;
+
+                case (2):
+                    botonSeleccionado = 2;
+                    btnLevantarReporte.Background = Brushes.White;
+                    break;
+
+                case (3):
+                    botonSeleccionado = 3;
+                    btnHistorialReportes.Background = Brushes.White;
+                    break;
+
+                case (4):
+                    botonSeleccionado = 4;
+                    btnChat.Background = Brushes.White;
+                    break;
+
+
+            }
+
+        }
+
+
     }
 }

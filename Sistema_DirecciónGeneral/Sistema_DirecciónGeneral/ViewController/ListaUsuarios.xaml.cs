78dd181d61737060c57496a116cac940a25f1aaa
Luis Fajardo <lrfs_13@hotmail.com>
2020-06-02 02:21:04 -0500

    Lista de usuarios y delegaciones
     sin funcionalidad  jeje

---
+++ Sistema_DirecciónGeneral/Sistema_DirecciónGeneral/ViewController/ListaUsuarios.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sistema_DirecciónGeneral.ViewController
+{
+    /// <summary>
+    /// Lógica de interacción para ListaUsuarios.xaml
+    /// </summary>
+    public partial class ListaUsuarios : UserControl
+    {
+        public ListaUsuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregarUsuario_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEditarUsuario_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
